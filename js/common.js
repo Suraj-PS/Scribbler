43bdfd98e2d9ad72e602aea56c963196a15f9b09
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2019-12-22 00:08:44 +0530

    Constructed SignIn Modal

--- js/common.js
+++ js/common.js
@@ -1,25 +1,51 @@
-// Get the modal
-var modal = document.getElementById("signUpModal");
+// SignUp Modal
+function displaySignUpModal() {
+    var modal = document.getElementById("signUpModal");
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("signUpButton");
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+    }
+    
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// SignIn Modal
+function displaySignInModal() {
+    var modal = document.getElementById("signInModal");
+    var span = document.getElementsByClassName("close")[1];
+
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+
+    }
+
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function switchToSignUpModal() {
+    var signInModal = document.getElementById("signInModal");
+    var signUpModal = document.getElementById("signUpModal");
+
+    if (signInModal.style.display === "block") {
+        signInModal.style.display = "none";
+        displaySignUpModal();
+    }
 }
