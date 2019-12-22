05f7d769fd58f46e5f8ebe90d0536a1e37731983
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2019-12-22 14:27:12 +0530

    Constructed Create Post Modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 function openBlogListPage() {
     window.location.href = "./html/bloglist.html";
 }
+
+function displayCreatePostModal() {
+    var modal = document.getElementById("createPostModal");
+    var span = document.getElementsByClassName("close")[2];
+
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+
+    }
+}
